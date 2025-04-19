cbcea1e35c1248a29e6468141d3b1415dd5bc9ad
Tiramister <contact@tiramister.net>
2025-04-19 11:05:36 +0900

    build: Dokka

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,6 +1,11 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URL
+
 plugins {
     id("java-library")
     kotlin("jvm") version "1.8.20"
+
+    id("org.jetbrains.dokka") version "2.0.0"
     id("com.diffplug.spotless") version "7.0.3"
 }
 
@@ -26,5 +31,16 @@ spotless {
     }
     kotlinGradle {
         ktlint("1.5.0")
+    }
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets.configureEach {
+        includes.from("docs/package.md")
+        sourceLink {
+            localDirectory.set(projectDir.resolve("src"))
+            remoteUrl.set(URL("https://github.com/Tiramister/cp-workspace-kt/tree/main/library/src"))
+            remoteLineSuffix.set("#L")
+        }
     }
 }
